79d09dff032c58c1de4324ac2b8bd5947bbcfff3
Neha Verma <nehbehl@gmail.com>
2024-11-29 19:47:38 +0530

    Delete lesson-1-name-of-lesson/exercises/solution/post-reader/src/app directory

--- lesson-1-name-of-lesson/exercises/solution/post-reader/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HistoryComponent } from './history/history.component';
-import { PostsComponent } from './posts/posts.component';
-
-export const routes: Routes = [
-    {path: '', component: PostsComponent},
-    {path: 'history', component: HistoryComponent}
-
-];
